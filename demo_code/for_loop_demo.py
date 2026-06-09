954691b1503329be4042a5c2dbc6ebea26c65ea8
Anish-Suddala <anishsuddala@gmail.com>
2026-06-09 11:34:42 -0500

    Added these files
    input_example
    input_example_improved
    input_example_improved_functions
    for_loop_demo

---
+++ demo_code/for_loop_demo.py
@@ -0,0 +1,20 @@
+def main():
+    #print integers between 0 and 10
+    for number in range(11):
+        print(number)
+    
+    #print integers between 5 and 10
+    print("\n\n")    
+    for number in range(5,11):
+        print(number)
+    
+    #print even integers between 0 and 10
+    print("\n\n")    
+    for number in range(0,11,2):
+        print(number)
+    #print odd integers between 0 and 10
+    print("\n\n")    
+    for number in range(1,11,2):
+        print(number)
+
+main()
