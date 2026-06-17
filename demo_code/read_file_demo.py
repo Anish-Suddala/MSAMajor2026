015fabd5235005c3d3366fbcd86e14922466e7b1
Anish-Suddala <anishsuddala@gmail.com>
2026-06-17 09:28:58 -0500

    Added menu.txt and updated read_file_demo

--- demo_code/read_file_demo.py
+++ demo_code/read_file_demo.py
@@ -6,12 +6,19 @@ def main():
     #create an empty dictionary
     menu_items = {}
     #use a loop to read the contents of the file line by line
-    for line_of_data in data_file:
+    for line_of_data in data_file:        
+        item_name_and_price = line_of_data.split(",")
         print(line_of_data)
-        line_of_data.split()
+
         #split the line at the comma
+        item_name = item_name_and_price[0]
+        item_price = float(item_name_and_price[1])
+
         #get the item and price from the list
         #create an entry in the dictionary for the item and price
     #close the file
-
+    menu_items[item_name] = item_price
+    data_file.close()
+    for items,price in menu_items.items():
+        print(f"{items}: ${price}")
 main()
