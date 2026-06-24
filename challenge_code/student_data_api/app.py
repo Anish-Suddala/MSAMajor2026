e7eadc8b8c3969fce7a29660bdb802f594270df4
Anish-Suddala <anishsuddala@gmail.com>
2026-06-24 11:28:40 -0500

    Update app.py in student_api

--- challenge_code/student_data_api/app.py
+++ challenge_code/student_data_api/app.py
@@ -13,11 +13,14 @@ Input: search_key - key in the dictionary we want to check the value of
     search_value - the value of the key we need to match
 Output: list of student dictionaries that match the search criteria
 """
+#loop through the dictionaries
 def search_dictionary_list(search_key,search_value):
     student_dictionaries = sg.get_student_dictionaries()
     category = []
     for line in student_dictionaries:
-        if(line[search_key] == search_value):
+    #determine if the search value matches the key values we are looking for
+        if(line[search_key].lower() == search_value.lower()):
+            #add the student dictionary to the results list
             category.append(line)
     return category
 
@@ -36,9 +39,24 @@ def api_all():
     return jsonify(student_dictionaries)
 
 #create a route that returns students of a specific major
+#api/majors/history
+@app.route('/api/majors/<string:major>',methods=['GET'])
+def api_students_by_major(major:str):
+    #call the search function to get students with this major
+    major_students = search_dictionary_list("major",major)
+    return jsonify(major_students)
 #create a route that returns students of a specific class(freshman,sophomore,junior,senior)
+#api/class/{the class we are looking for}
+@app.route('/api/class/<string:class>',methods=['GET'])
+def api_students_by_class(sclass:str):
+    class_students = search_dictionary_list("class",sclass)
+    return jsonify(class_students)
+
 #create a route that returns a specific student by ID
+@app.route('/api/student/id/<string:id>',methods=['GET'])
+def api_get_student_by_id(id:str):
+    student = search_dictionary_list("id",id)
+    return jsonify(student)
 
     #run the application
 app.run(debug=True)
-
