00a76b6264a69e0323ec508dc2bba3e910cc7c12
Anish-Suddala <anishsuddala@gmail.com>
2026-06-26 10:17:30 -0500

    V3 of API and Webdev

---
+++ student_web_app/app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, url_for, redirect, abort, flash
+import requests
+
+#make a Flask app
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+#set secret key
+app.config["SECRET_KEY"] = "your secret key"
+
+"""
+ Function to request student data from the api
+Input: url
+Output: JSON student data
+"""
+def get_student_data(url:str):
+    #make a request
+    response = requests.get(url)
+    #convert format to json
+    response_json = response.json()
+    #return the response
+    return response_json
+
+#create a route for the website index/root/homepage
+@app.route('/',methods=['GET'])
+def index():
+    #make a request to the student data api for all students
+    url = "http://127.0.0.1:5000/api/students/all"
+
+    #get the student data
+    student_data = get_student_data(url)
+    return render_template("index.html",student_data=student_data)
+
+#create a route for the majors search page to respond to get requests
+@app.route('/majors',methods=['GET'])
+def majors_get():
+    #get the list of majors
+    url = "http://127.0.0.1:5000/api/majors/all"
+    major_list = get_student_data(url)
+    #send the list of majors to the majors template to populate the menu
+    return render_template('majors.html',major_list=major_list)
+
+#create a route for the majors search page to respond to 
+# POST requests after the form is submitted
+@app.route('/majors',methods=['POST'])
+def majors_post():
+    #get the list of majors
+    url = "http://127.0.0.1:5000/api/majors/all"
+    major_list = get_student_data(url)
+    major = request.form.get('major')
+
+    #if major input is invalid, display error message and reload page
+    if major == "":
+        flash("ERROR: You must select a major")
+        return redirect(url_for('majors_get'))
+
+    url = f"http://127.0.0.1:5000/api/majors/{major}"
+    result_list = get_student_data(url)
+    print(result_list)
+    return render_template('majors.html',major_list=major_list,result_list=result_list, major=major)
+#run the flask app
+app.run(port=5001)
